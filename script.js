53c70400d2276960055fa1ed5e6b6e606dbd0c04
rage73 <raghavagrawal2000ra@gmail.com>
2021-07-06 20:55:33 +0530

    button style

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 let value1 = document.getElementById('value1');
 let value2 = document.getElementById('value2');
 let value3 = document.getElementById('value3');
-
-let btnStop = document.getElementById('btnStop');
-let inpSpeed = document.getElementById('inpSpeed');
+let btnSpin = document.getElementById('btnSpin');
 
 let values = [
     '😃', '😎', '😇', '🤣', '🤩', '😮', '🤑', '💀', '😴'
@@ -28,18 +26,21 @@ function updateAnimation(newSpeed) {
     }, 1000/newSpeed);
 }
 
-btnStop.onclick = function () {
+btnSpin.onclick = function () {
     if(this.innerHTML === 'STOP') {
         this.innerHTML = 'SPIN';
         clearInterval(animationId);
         value1.classList.remove('animate');
         value2.classList.remove('animate');
         value3.classList.remove('animate');
+        btnSpin.classList.remove('stop');
+
     } else {
         this.innerHTML = 'STOP';
         updateAnimation(5);
         value1.classList.add('animate');
         value2.classList.add('animate');
         value3.classList.add('animate');
+        btnSpin.classList.add('stop');
     }
 }
